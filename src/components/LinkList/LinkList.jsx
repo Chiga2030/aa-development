9c822254df2b3bf636658e67c425f46ee7eb2153
Anton Tkachuk <chiga2030@gmail.com>
2021-05-31 08:01:27 +0300

    refactoring classnames

--- src/components/LinkList/LinkList.jsx
+++ src/components/LinkList/LinkList.jsx
@@ -1,12 +1,14 @@
 import styles from './LinkList.module.css';
 
+import classNames from 'classnames';
+
 const LinkList = props => (
-  <ul className={`${styles.linkList} ${props.className}`}>
-    <li className={styles.listItem}>
-      <a className={styles.link} href="#">Ссылка на проект</a>
+  <ul className={ classNames(styles.linkList, props.className) }>
+    <li className={ styles.listItem }>
+      <a className={ styles.link } href="#">Ссылка на проект</a>
     </li>
-    <li className={styles.listItem}>
-      <a className={styles.link} href="#">Репозиторий на Github</a>
+    <li className={ styles.listItem }>
+      <a className={ styles.link } href="#">Репозиторий на Github</a>
     </li>
   </ul>
 );
