e9a95a3c90126c25c7fee4d2ee51760788faa8fd
Anton Tkachuk <chiga2030@gmail.com>
2021-06-04 13:23:08 +0300

    refactoring

--- src/components/UseScroll/UseScroll.jsx
+++ src/components/UseScroll/UseScroll.jsx
@@ -6,20 +6,24 @@ import classNames from 'classnames';
 import BulletsContainer from '../Bullets/BulletsContainer';
 
 
-const UseScroll = props => {
-  console.log('props', props);
-  return (
-    <div className={styles.wrapper}>
-      <img
-        className={classNames(styles.mouse, styles.marginBottom)}
-        src={mouse}
-        alt="computer mouse" />
-      <small
-        className={ classNames(styles.smallText, styles.marginBottom) }
-      >use scroll</small>
-      <BulletsContainer amountBullest = { props.amountBullest } />
-    </div>
-  );
-};
+const UseScroll = () => (
+  <div className={styles.wrapper}>
+    <img
+      className={classNames(styles.mouse, styles.marginBottom)}
+      src={mouse}
+      alt="computer mouse" />
+    <small
+      className={
+        classNames(
+          styles.smallText,
+          styles.marginBottom
+        )
+      }
+    >
+      use scroll
+    </small>
+    <BulletsContainer />
+  </div>
+);
 
 export default UseScroll;
